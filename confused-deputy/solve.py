057e152239f96ac81bde50253f3c8b2bc370388c
ChoKyuWon <yyj10137@gmail.com>
2020-06-18 11:31:23 +0000

    confused-deputy

--- confused-deputy/solve.py
+++ confused-deputy/solve.py
@@ -1,16 +1,4 @@
-from pwn import *
 from Crypto.Hash import SHA256
 
-
-target = "../../../confused-deputy/confused-deputy"
-p = process(target)
-
-p.recvuntil(':')
-p.sendline("passwd")
-h = SHA256.new('apple')
-p.sendline(h.hexdigest())
-
-p = process(target)
-p.sendline("passwd")
-p.sendline("apple")
-p.interactive()
+h = SHA256.new(b'apple')
+print(h.hexdigest())
